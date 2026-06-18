925b1fb2a2c637dbce1ab81f666dfbe06e8c597f
alexperezortuno <alexperezortuno@gmail.com>
2026-06-18 16:25:42 -0400

    feat: add Google Analytics support via GA_MEASUREMENT_ID

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 : "${APP_URL:=https://example.com}"
 : "${PORT:=8085}"
 
-envsubst '$APP_URL' < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html.tmp
+envsubst '$APP_URL $GA_MEASUREMENT_ID' < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html.tmp
 mv /usr/share/nginx/html/index.html.tmp /usr/share/nginx/html/index.html
 
 envsubst '$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
